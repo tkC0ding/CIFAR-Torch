0ade347d6eb9972a89838a4c5f84ca4a05417502
Toshan Kirit Jagani <tkjagani@gmail.com>
2024-06-08 00:07:15 +0530

    Defined device and created data loaders

--- code_file.py
+++ code_file.py
@@ -17,3 +17,16 @@ testing_data = datasets.CIFAR10(
     download = True,
     transform = ToTensor()
 )
+
+batch_size = 64
+
+train_loader = DataLoader(dataset = training_data, batch_size = batch_size, shuffle=True)
+test_loader = DataLoader(dataset = testing_data, batch_size = batch_size, shuffle=False)
+
+device = torch.device(
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps"
+    if torch.backends.mps.is_available()
+    else "cpu"
+)
