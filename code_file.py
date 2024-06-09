4e83f26bf84daeea77835fa673da575e95a29824
Toshan Kirit Jagani <tkjagani@gmail.com>
2024-06-09 23:51:32 +0530

    Trying transfer learning aapproach
    Not gonna push the data to github cuz I have reached the data quota for lfs objects : (

--- code_file.py
+++ code_file.py
@@ -1,113 +1,105 @@
 import torch
 from torch import nn
-from torchvision import datasets
-import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
-import numpy as np
-import matplotlib.pyplot as plt
+from torchvision import datasets, models
+import torchvision.transforms as transforms
 
-device = (
-    "cuda"
-    if torch.cuda.is_available()
-    else "mps"
-    if torch.backends.mps.is_available()
-    else "cpu"
+batch_size = 64
+learning_rate = 0.001
+num_epochs = 5
+
+# Choose device
+device = torch.device(
+    "cpu"
 )
 
-num_epochs = 11
-batch_size = 10
-learning_rate = 0.005
+#Data preprocessing
+train_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+    transforms.RandomHorizontalFlip(),
+    transforms.RandomRotation(10),
+    transforms.Resize(256)
+])
 
-transform_train = transforms.Compose([transforms.ToTensor(),
-                                      transforms.RandomHorizontalFlip(), transforms.RandomCrop(32),
-                                      transforms.Grayscale(),
-                                      transforms.Normalize(0.5, 0.5)])
+test_transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
+    transforms.Resize(256)
+])
 
-transform_test = transforms.Compose([transforms.ToTensor(),
-                                     transforms.Grayscale(),
-                                     transforms.Normalize(0.5, 0.5)])
-
+# Downloading datasets
 train_dataset = datasets.CIFAR10(
-    root = 'Data',
+    root='Data',
     train = True,
     download = True,
-    transform=transform_train
+    transform = train_transform
 )
 
 test_dataset = datasets.CIFAR10(
-    root = 'Data',
+    root='Data',
     train = False,
     download = True,
-    transform=transform_test
+    transform = test_transform
 )
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+# Creating data loaders
+train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
+test_loader = DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
 
-classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+# Loading alexnet
+model = models.alexnet(weights=True).to(device)
 
-class CNN(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.r = nn.ReLU()
-        self.pool = nn.AvgPool2d(2,2)
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(400, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
-    
-    def forward(self, x):
-        x = self.conv1(x)
-        x = self.r(x)
-        x = self.pool(x)
+# Freezing model parameters
+for params in model.parameters():
+    params.requires_grad = False
 
-        x = self.conv2(x)
-        x = self.r(x)
-        x = self.pool(x)
+# Change the final layer
+num_features = model.classifier[6].in_features
+model.classifier[6] = nn.Linear(num_features, 10)
 
-        x = self.flatten(x)
-        x = self.fc1(x)
-        x = self.r(x)
-        x = self.fc2(x)
-        x = self.r(x)
-        x = self.fc3(x)
-        return(x)
-
-model = CNN().to(device)
+# Compile
 loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-num_steps = len(train_loader)
+print(f"Using {device} device.")
 
-for epoch in range(num_epochs):
-    for i, (X, y) in enumerate(train_loader):
-        X, y = X.to(device), y.to(device)
+print(model)
 
-        y_pred = model(X)
-        loss = loss_fn(y_pred, y)
+# train function
+def train(model, loss_fn, optimizer, dataloader):
+    model.train()
+    num_steps = len(dataloader)
+    for epoch in range(num_epochs):
+        for i, (X, y) in enumerate(dataloader):
+            X, y = X.to(device), y.to(device)
 
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            y_pred = model(X)
+            loss = loss_fn(y_pred, y)
 
-        if (i + 1) % 2000 == 0:
-            print(f"Epoch : [ {epoch+1}/{num_epochs} ], Step [ {i+1}/{num_steps} ], Loss: {loss.item}")
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
 
-print('Finished Training!')
+            if (i + 1) % 782 == 0:
+                print(f"Epoch [ {epoch+1}/{num_epochs} ] Step [ {i+1}/{num_steps} ] Loss: {loss.item()}")
+    print("Training Finished!")
 
-with torch.no_grad():
-    n_correct = 0
-    n_samples = 0
-    n_class_correct = [0 for i in range(10)]
-    n_class_samples = [0 for i in range(10)]
-    for X,y in test_loader:
-        X, y = X.to(device), y.to(device)
-        
-        pred = model(X)
-        score, prediction = torch.max(pred, 1)
-        n_samples += y.size(0)
-        n_correct += (prediction == y).sum().item()
 
-        
+# validation function
+def validate(model, dataloader):
+    with torch.no_grad():
+        n_correct = 0
+        n_samples = 0
+        for X,y in dataloader:
+            X, y = X.to(device), y.to(device)
+
+            pred = model(X)
+            scores, predictions = torch.max(pred, 1)
+            n_correct += (predictions == y).sum().item()
+            n_samples += y.size(0)
+        acc = (n_correct/n_samples)*100
+        print("Accuracy :", acc)
+
+train(model, loss_fn, optimizer, train_loader)
+validate(model, test_loader)
