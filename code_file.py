eb9cdb62d759b2312c2b3859f0eff38dfbb27ba3
Toshan Kirit Jagani <tkjagani@gmail.com>
2024-06-17 17:56:01 +0530

    Restarting

--- code_file.py
+++ code_file.py
@@ -1,7 +1,7 @@
 import torch
 from torch import nn
 from torch.utils.data import DataLoader
-from torchvision import datasets, models
+from torchvision import datasets
 import torchvision.transforms as transforms
 
 batch_size = 64
@@ -10,61 +10,51 @@ num_epochs = 5
 
 # Choose device
 device = torch.device(
-    "cpu"
+    "cuda"
+    if torch.cuda.is_available()
+    else "mps" if torch.backends.mps.is_available()
+    else "cpu"
 )
-
-#Data preprocessing
-train_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomRotation(10),
-    transforms.Resize(256)
-])
-
-test_transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize([0.5, 0.5, 0.5], [0.5, 0.5, 0.5]),
-    transforms.Resize(256)
-])
 
 # Downloading datasets
 train_dataset = datasets.CIFAR10(
     root='Data',
     train = True,
     download = True,
-    transform = train_transform
+    transform = transforms.ToTensor()
 )
 
 test_dataset = datasets.CIFAR10(
     root='Data',
     train = False,
     download = True,
-    transform = test_transform
+    transform = transforms.ToTensor()
 )
 
 # Creating data loaders
 train_loader = DataLoader(dataset = train_dataset, batch_size = batch_size, shuffle = True)
 test_loader = DataLoader(dataset = test_dataset, batch_size = batch_size, shuffle = False)
 
-# Loading alexnet
-model = models.alexnet(weights=True).to(device)
+class CNN:
+    def __init__(self):
+        super().__init__()
+        self.relu = nn.LeakyReLU()
+        self.conv1 = nn.Conv2d(3, 16, 5, padding=2)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.dropout = nn.Dropout2d(0.5)
+        self.flatten = nn.Flatten()
+        self.conv2 = nn.Conv2d(16, 16, 5, padding=2)
+        self.conv3 = nn.Conv2d(16, 32, 5, padding=2)
+        self.conv4 = nn.Conv2d(32, 32, 5, padding=2)
+        self.conv5 = nn.Conv2d(32, 64, 3, padding=1)
+        self.conv6 = nn.Conv2d(64, 64, 3, padding=1)
+        self.Dense1 = nn.Linear(1024, 4096)
+        self.Dense2 = nn.Linear(4096, 4096)
+        self.Dense3 = nn.Linear(4096, 10)
 
-# Freezing model parameters
-for params in model.parameters():
-    params.requires_grad = False
+        nn.ModuleList([self.relu, self.conv1, self.conv2, self.conv3, self.conv4, self.conv5, self.conv6, self.Dense1, self.Dense2,
+                       self.Dense3, self.dropout, self.flatten, self.pool])
 
-# Change the final layer
-num_features = model.classifier[6].in_features
-model.classifier[6] = nn.Linear(num_features, 10)
-
-# Compile
-loss_fn = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-
-print(f"Using {device} device.")
-
-print(model)
 
 # train function
 def train(model, loss_fn, optimizer, dataloader):
